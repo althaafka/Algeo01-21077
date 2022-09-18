5c84c71bdc294e3a6bf822ef1eb78544126d3a3d
althaafka <althaafka004@gmail.com>
2022-09-18 22:08:53 +0700

    update Main.java, Menu.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 class Main extends Menu{
     public static void main(String[] args){
+        int opt;
         displayMenu();
+        opt = optionInput(1, 7);
+        switch(opt){
+            case 1:
+                displayMenuSPL();
+                opt = optionInput(1, 4);
+                break;
+            case 2:
+                displayMenuDet();
+                opt = optionInput(1, 2);
+                break;
+            case 3:
+                displayMenuInverse();
+                opt = optionInput(1, 2);
+                break;
+        }
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Menu {
-    public static void displayMenu(){
+    static Scanner scan = new Scanner(System.in);  
+    
+    public static void displayMenu(){      
         // Menampilkan main menu
         System.out.println("Main Menu");
         System.out.println("1. Sistem Persamaan Linear");
@@ -9,6 +13,44 @@ public class Menu {
         System.out.println("5. Interpolasi Bicubic");
         System.out.println("6. Regresi Linear Berganda");
         System.out.println("7. Keluar");
+    }
 
+    public static int optionInput(int min, int max){
+        //Mengembalikan opsi dari user
+        int opt;
+        System.out.println("Input menu "+ min + " sampai "+max);
+        System.out.print(">>> ");
+        opt = scan.nextInt();
+        while (opt<min || opt>max){
+            System.out.print("Input salah, Ulangi!\n");
+            System.out.print(">>> ");
+            opt = scan.nextInt();
+        }
+        System.out.println();
+        return opt;
+    }
+
+    public static void displayMenuSPL(){
+        // Menampilkan submenu SPL
+        System.out.println("Sistem Persamaan Liniear");
+        System.out.println("1. Eliminasi Gauss");
+        System.out.println("2. Eliminasi Gauss-Jordan");
+        System.out.println("3. Matriks Inverse");
+        System.out.println("4. Metode Cramer");
+    }
+
+    public static void displayMenuDet(){
+        // Menampilkan submenu SPL
+        System.out.println("Determinan");
+        System.out.println("1. Reduksi Baris");
+        System.out.println("2. Ekspansi Kofaktor");
+    }
+
+    public static void displayMenuInverse(){
+        // Menampilkan submenu SPL
+        System.out.println("Inverse Matrix");
+        System.out.println("1. Eliminasi Gauss-Jordan" );
+        System.out.println("2. Metode Kofaktor");
     }
 }
+
