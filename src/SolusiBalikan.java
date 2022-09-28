f1e344f2882cef848354ebde5e981685cadc2d7a
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2022-09-28 22:16:33 +0700

    update SolveBalikan

--- src/SolusiBalikan.java
+++ src/SolusiBalikan.java
@@ -1,5 +1,15 @@
 public class SolusiBalikan extends Matrix{
-    public static double[][] SolveBalikan(double[][] A, double[][] b){
+    public static double[][] SolveBalikan(double[][] A){
+        // Prekondisi A selalu augmented
+        // x = A^(-1)b
+
+        // Buat matriks b terpisah;
+        double[][]
+        b = createMatrix(nBaris(A), 1);
+        for (int i=0; i<nBaris(A); i++){
+            b[i][0] = A[i][nKolom(A)-1];
+        }
+        // Ubah matriks menjadi matriks persegi
         A = AugmentedtoSquare(A);
         double[][] Ainverse = Invers.InversCofactor(A);
         double[][] x = kaliMatriks(Ainverse, b);
